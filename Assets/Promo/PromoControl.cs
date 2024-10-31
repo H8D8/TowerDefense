f7ae92b2d498bb1f07783070621977bff3e2856c
H8D8 <h8di@outlook.com>
2024-10-31 23:16:10 +0330

    Initial commit

---
+++ Assets/Promo/PromoControl.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PromoControl : MonoBehaviour {
+
+	public Animator anim;
+	public int clip;
+
+	// Use this for initialization
+	void Start () {
+
+		anim = GetComponent<Animator> ();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetButtonDown ("Fire2"))
+			clip++;
+
+	
+		anim.SetInteger ("turret", clip);
+		
+	}
+}
